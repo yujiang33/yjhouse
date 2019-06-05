5520db2b3b27ca4b06518f432a54e06eb2d44603
yj3381 <771152019@qq.com>
2019-06-05 11:13:57 +0800

    django项目 数据库 后台管理

--- demo1/booktest/admin.py
+++ demo1/booktest/admin.py
@@ -1,3 +1,32 @@
+"""
+Django强大的后台管理
+应用的admin.pyz注册模型类
+"""
 from django.contrib import admin
+from.models import BookInfo,HereInfo
 
 # Register your models here.
+# admin.site.register(BookInfo)
+# admin.site.register(HereInfo)
+
+class HereInfoInlines(admin.StackedInline):
+    model =HereInfo
+    extra =1
+class BookInfoAdmin(admin.ModelAdmin):
+    # 重写list_display 重写后台显示那些字段
+    list_display =('title','pub_date')
+    list_filter =('title','pub_date')
+    list_per_page =20
+    inlines = [HereInfoInlines]
+# 在注册模型时，注册该模型的后台管理类 通过管理类重写后台
+admin.site.register(BookInfo,BookInfoAdmin)
+
+class HereInfoAdmin(admin.ModelAdmin):
+    list_display =('name','content')
+    list_filter =('name',) #元组加，
+    search_fields =('name','content')
+admin.site.register(HereInfo,HereInfoAdmin)
+
+
+
+
--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -8,9 +8,15 @@ class BookInfo(models.Model):
     title =models.CharField(max_length=20)
     pub_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.title
+
 class HereInfo(models.Model):
     name = models.CharField(max_length=20)
     gender = models.BooleanField(default=True)
     content = models.CharField(max_length=100)
     book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
     # book作为外键关联到bookinfo表
+# 配置成中文
+    def __str__(self):
+        return self.name
